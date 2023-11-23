0fffd99074fb37b137041153d2243cf11f56f243
Danilo Queiroz <daniloqueirozsilva05@gmail.com>
2023-11-22 23:33:25 -0300

    Inicio da criação do front end para ToDo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: String='Minhas Tarefas';
+
+  constructor() {
+
+    this.todos.push(new Todo(1, 'Passear com o cahorro', false));
+    this.todos.push(new Todo(2, 'Estudar Angular', false))
+    this.todos.push(new Todo(3, 'Revisar Matéria', true));
+  }
+
+  remove(todo: Todo){
+    const index = this.todos.indexOf(todo);
+    if(index != -1){
+      this.todos.splice(index, 1)
+    }
+  }
+
+  markAsDone(){
+
+  }
+
+  markAsUndone(){
+
+  }
+
 }
+
